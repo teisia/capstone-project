82b013f47a2b572e6433070721c11f2173d87474
Teisia Park <teisia@gmail.com>
2016-04-05 17:47:26 -0600

    setup basic routes in angular

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var trips = require('./routes/trips');
 
 var app = express();
 
@@ -32,6 +33,7 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/trips', trips);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/trips.js
+++ routes/trips.js
@@ -11,4 +11,21 @@ function verifyToken(user) {
   return jwt.verify(user, process.env.TOKEN_SECRET)
 }
 
+function trips() {
+  return knex('trip');
+};
+
+function tripD() {
+  return knex('trip_details');
+};
+
+function tasks() {
+  return knex('tasks');
+};
+
+function user() {
+  return knex('user');
+};
+
+
 module.exports = router;

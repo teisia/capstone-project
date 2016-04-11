f489b10d2010ad428dcba49bf539c95915c34805
Teisia Park <teisia@gmail.com>
2016-04-11 17:55:14 -0600

    redirect working after auth

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,22 @@
 app.controller('LoginCtrl', function($scope, $auth, $location) {
 
   $scope.authenticate = function(provider) {
-  $auth.authenticate(provider);
+    $auth.authenticate(provider)
+      .then(function(response) {
+        console.log('You have successfully signed in with ' + provider + '!');
+        $location.path('/dashboard')
+      })
+      .catch(function(error) {
+        if (error.error) {
+          // Popup error - invalid redirect_uri, pressed cancel button, etc.
+          console.log(error.error);
+        } else if (error.data) {
+          // HTTP response error from server
+        console.log(error.data.message, error.status);
+        } else {
+          console.log(error);
+        }
+      });
   };
 
 });
